3e5a23d9a9785070ca820f2bfe6c669118ed1180
GiangBeo <giangbeoit@gmail.com>
2020-11-12 20:04:30 +0700

    Update

--- src/WebikeCoreService.php
+++ src/WebikeCoreService.php
@@ -3,6 +3,7 @@
 namespace Webikevn\Cors;
 
 use Asm89\Stack\CorsService;
+use Symfony\Component\HttpFoundation\Request;
 
 class WebikeCoreService extends CorsService
 {
@@ -13,5 +14,14 @@ class WebikeCoreService extends CorsService
     public function isCorsRequest(Request $request)
     {
         return config('webike_cors.enable_cors_check') && !$this->isSameHost($request);
+    }
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    private function isSameHost(Request $request)
+    {
+        return $request->headers->get('Origin') === $request->getSchemeAndHttpHost();
     }
 }
